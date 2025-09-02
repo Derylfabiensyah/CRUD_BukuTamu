7f5dd11eaf6522e9d7071e7ac05ed13f417859f9
= <=>
2025-09-02 19:24:33 +0700

    Koneksi

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+
+define('HOSTNAME', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+define('DB_NAME', 'app_bukutamu');
+
+$koneksi = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DB_NAME);
+
+?>
