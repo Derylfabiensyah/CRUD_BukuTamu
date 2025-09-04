93238146c7683e266f45605095512d3fa095be80
= <=>
2025-09-04 19:22:03 +0700

    menambahkan fungsi edit dan hapus data tamu

--- function.php
+++ function.php
@@ -10,4 +10,56 @@ function query($query) {
   }
   return $rows;
 }
+
+function tambah_tamu($data){
+  global $koneksi;
+
+  $kode = htmlspecialchars($data['id_tamu']);
+  $tanggal = date('Y-m-d');
+  $nama_tamu = htmlspecialchars($data['nama_tamu']);
+  $alamat = htmlspecialchars($data['alamat']);
+  $no_hp = htmlspecialchars($data['no_hp']);
+  $bertemu = htmlspecialchars($data['bertemu']);
+  $kepentingan = htmlspecialchars($data['kepentingan']);
+
+  $query = "INSERT INTO tabel_bukutamu 
+            (id_tamu, tanggal, nama_tamu, alamat, no_hp, bertemu, kepentingan) 
+            VALUES 
+            ('$kode', '$tanggal', '$nama_tamu', '$alamat', '$no_hp', '$bertemu', '$kepentingan')";
+  
+  mysqli_query($koneksi, $query);
+
+  return mysqli_affected_rows($koneksi);
+}
+
+function ubah_tamu($data){
+  global $koneksi;
+
+  $id = htmlspecialchars($data['id_tamu']);
+  $nama = htmlspecialchars($data['nama_tamu']);
+  $alamat = htmlspecialchars($data['alamat']);
+  $no_hp = htmlspecialchars($data['no_hp']);
+  $bertemu = htmlspecialchars($data['bertemu']);
+  $kepentingan = htmlspecialchars($data['kepentingan']);
+
+  $query = "UPDATE tabel_bukutamu SET
+              nama_tamu = '$nama',
+              alamat = '$alamat',
+              no_hp = '$no_hp',
+              bertemu = '$bertemu',
+              kepentingan = '$kepentingan'
+            WHERE id_tamu = '$id'";
+
+  mysqli_query($koneksi, $query);
+
+  return mysqli_affected_rows($koneksi);
+}
+
+function hapus_tamu($id) {
+  global $koneksi;
+  $query = "DELETE FROM tabel_bukutamu WHERE id_tamu = '$id'";
+  mysqli_query($koneksi, $query);
+  return mysqli_affected_rows($koneksi);
+}
+  
 ?>
